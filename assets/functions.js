b26985043985927cb5054b218d50cbd7971856c1
Zac <zac.kahl@gmail.com>
2021-01-26 15:34:29 +1000

    Updated Copyright
    Updated copyright to display current year.

--- assets/functions.js
+++ assets/functions.js
@@ -106,6 +106,11 @@ window.onload = function () {
 		},
 		"retina_detect": true
 	});
+
+	// Get current year for copyright
+	var date = new Date();
+	var year = date.getFullYear();
+	document.getElementById("year").innerHTML = year;
 };
 
 // -- Source code (https://github.com/download13/blockies) --------------------------------
